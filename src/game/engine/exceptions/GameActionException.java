16ac6506768b6b3b61e2dea5acdafde4779570ca
CasuallyC0ding <140198456+CasuallyC0ding@users.noreply.github.com>
2024-02-28 21:28:00 +0200

    Add files via upload

---
+++ src/game/engine/exceptions/GameActionException.java
@@ -0,0 +1,13 @@
+package engine.exceptions;
+
+abstract public class GameActionException extends Exception {
+
+	public GameActionException() {
+		super();
+	}
+	
+	public GameActionException(String message) {
+		super(message);
+	}
+
+}
---
+++ src/game/engine/exceptions/InvalidCSVFormat.java
@@ -0,0 +1,30 @@
+package engine.exceptions;
+
+import java.io.IOException;
+
+public class InvalidCSVFormat extends IOException{
+
+	private static final String MSG = "Invalid input detected while reading csv file, input = \n";
+	private String inputLine; // read & write
+	
+	public InvalidCSVFormat(String inputLine) {
+        super(MSG + inputLine);
+        this.inputLine = inputLine;
+    }
+	
+	public InvalidCSVFormat(String message, String inputLine) {
+        super(message);
+        this.inputLine = inputLine;
+    }
+
+	public String getInputLine() {
+		return inputLine;
+	}
+
+	public void setInputLine(String inputLine) {
+		this.inputLine = inputLine;
+	}
+
+	
+	
+}
